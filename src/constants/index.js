f8531af4050fc3a2600b3629c4767c730cd54c8f
Vitalik <vitalik.vovryk@gmail.com>
2020-10-22 20:52:23 +0300

    Took out constants to another file

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  NOT_FOUND: 'Not found',
+  BAD_REQUEST: 'Bad request',
+  STATUS: {
+    Ok: 200,
+    BadRequest: 400,
+    NotFound: 404,
+  },
+};
--- src/controllers/goods.js
+++ src/controllers/goods.js
@@ -1,45 +1,46 @@
 const Goods = require('../db/goodsService');
+const constants = require('../constants/index');
 
 const createGoods = async (req, res) => {
   const goodsData = req.body;
   const goods = await new Goods().createGoods(goodsData);
-  return res.status(200).json(goods);
+  return res.status(constants.STATUS.Ok).json(goods);
 };
 
 const deleteGoods = async (req, res) => {
   const { id } = req.params;
-  if (!id) return res.status(400).json('BAD_REQUEST');
+  if (!id) return res.status(constants.STATUS.BadRequest).json(constants.BAD_REQUEST);
   const findDeleteGoods = await new Goods().getOneGoods(id);
   if (findDeleteGoods) {
     const deletedGoods = await new Goods().deleteGoodsById(id);
-    return res.status(200).json(deletedGoods);
+    return res.status(constants.STATUS.Ok).json(deletedGoods);
   }
-  return res.status(404).json('NOT_FOUND');
+  return res.status(constants.STATUS.NotFound).json(constants.NOT_FOUND);
 };
 
 const updateGoods = async (req, res) => {
   const goodsData = req.body;
   const { id } = req.params;
-  if (!id) return res.status(400).json('BAD_REQUEST');
+  if (!id) return res.status(constants.STATUS.BadRequest).json(constants.BAD_REQUEST);
   const findUpdateGoods = await new Goods().getOneGoods(id);
   if (findUpdateGoods) {
     const updatedGoods = await new Goods().updateGoods(id, goodsData);
-    return res.status(200).json(updatedGoods);
+    return res.status(constants.STATUS.Ok).json(updatedGoods);
   }
-  return res.status(404).json('NOT_FOUND');
+  return res.status(constants.STATUS.NotFound).json(constants.NOT_FOUND);
 };
 
 const getOneGoods = async (req, res) => {
   const { id } = req.params;
-  if (!id) return res.status(400).json('BAD_REQUEST');
+  if (!id) return res.status(constants.STATUS.BadRequest).json(constants.BAD_REQUEST);
   const findOne = await new Goods().getOneGoods(id);
-  if (findOne) return res.status(200).json(findOne);
-  return res.status(404).json('NOT_FOUND');
+  if (findOne) return res.status(constants.STATUS.Ok).json(findOne);
+  return res.status(constants.STATUS.NotFound).json(constants.NOT_FOUND);
 };
 
 const getAllGoods = async (req, res) => {
   const findAll = await new Goods().getAllGoods();
-  return res.status(200).json(findAll);
+  return res.status(constants.STATUS.Ok).json(findAll);
 };
 
 module.exports = {
